cd2334e663cf76d12eed38df334319d489bdffa0
Alexis Guerin <aguerin@student.42.fr>
2017-02-03 09:54:30 +0100

    Ajout de la fonction ft_lstdelall

---
+++ ft_lstdelall.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelall.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguerin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/03 09:49:39 by aguerin           #+#    #+#             */
+/*   Updated: 2017/02/03 09:49:42 by aguerin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+/*
+** Suppression de toute la liste (libération de la mémoire allouée).
+*/
+
+void	dellist(t_list *list)
+{
+	t_list *new;
+
+	while (list)
+	{
+		new = list->next;
+		free(list->content);
+		list->content = NULL;
+		free(list);
+		list = new;
+	}
+	list = NULL;
+}
