93a1b176d2e424e1b09be5be2c777932c8974d59
Alexis Guerin <aguerin@student.42.fr>
2017-03-10 15:30:32 +0100

    Modification header

--- ft_putwchar.c
+++ ft_putwchar.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putwchar.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aguerin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: aguerin <aguerin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/10 14:52:03 by aguerin           #+#    #+#             */
 /*   Updated: 2017/03/10 14:52:05 by aguerin          ###   ########.fr       */
