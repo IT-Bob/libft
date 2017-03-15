013fd571c3d2052d6516f2e2a53bfa21f509edd8
Alexis Guerin <aguerin@student.42.fr>
2017-03-15 11:16:54 +0100

    Ajout de ft_putwnchar() et correction de commentaires dans ft_putnchar()

--- src/print/ft_putwnchar.c
+++ src/print/ft_putwnchar.c
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnchar.c                                      :+:      :+:    :+:   */
+/*   ft_putwnchar.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aguerin <aguerin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/15 10:44:33 by aguerin           #+#    #+#             */
-/*   Updated: 2017/03/15 10:44:36 by aguerin          ###   ########.fr       */
+/*   Created: 2017/03/15 11:10:49 by aguerin           #+#    #+#             */
+/*   Updated: 2017/03/15 11:11:07 by aguerin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** ft_putnchar() affiche les n premiers caractères de la chaîne str.
+** ft_putwnchar() affiche les n premiers caractères de la chaîne str.
 ** Si n est plus grand que la taille de la chaîne, l'affichage s'arrête en fin
 ** de chaîne.
 ** Si str est NULL, la fonction quitte en affichant une erreur.
@@ -20,14 +20,14 @@
 
 #include "libft.h"
 
-int	ft_putnchar(char *str, unsigned int n)
+int	ft_putwnchar(wchar_t *str, unsigned int n)
 {
 	int	cmp;
 
 	if (!str)
-		ft_error("Erreur : Chaîne NULL... - ft_putnchar()", -1);
+		ft_error("Erreur : Chaîne NULL... - ft_putwnchar()", -1);
 	cmp = 0;
 	while (*str && n-- > 0)
-		cmp += ft_putchar(*(str++));
+		cmp += ft_putwchar(*(str++));
 	return (cmp);
 }
