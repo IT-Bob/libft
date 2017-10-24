9590311811e059ec9f6f97ae38386e809c1e53bf
Alexis Guerin <aguerin@student.42.fr>
2017-10-24 15:56:30 +0200

    Ajout d'un paramètre pour une liste doublement chaînée

--- src/list/ft_lstnew.c
+++ src/list/ft_lstnew.c
@@ -23,8 +23,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*list;
 
-	list = (t_list*)malloc(sizeof(t_list));
-	if (list)
+	if ((list = (t_list*)malloc(sizeof(t_list))))
 	{
 		if (!content)
 		{
@@ -38,8 +37,8 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 			ft_memcpy(list->content, content, content_size);
 			list->content_size = content_size;
 		}
+		list->prev = NULL;
 		list->next = NULL;
-		return (list);
 	}
-	return (NULL);
+	return (list);
 }
