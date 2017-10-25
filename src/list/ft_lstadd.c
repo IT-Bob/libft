bf32731237d3b507ff3e192d042c0b11c4de9574
Alexis Guerin <aguerin@student.42.fr>
2017-10-25 09:23:06 +0200

    Gestion de la double chaîne

--- src/list/ft_lstadd.c
+++ src/list/ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: aguerin <aguerin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/18 11:36:41 by aguerin           #+#    #+#             */
-/*   Updated: 2016/11/18 15:40:00 by aguerin          ###   ########.fr       */
+/*   Updated: 2017/10/25 09:20:12 by aguerin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	ft_lstadd(t_list **alst, t_list *new)
 	t_list *lst;
 
 	lst = *alst;
+	lst->prev = new;
 	new->next = lst;
 	*alst = new;
 }
